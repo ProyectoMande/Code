a2f2208f37eaf762d1b97b6a0564713f5f1091cc
Bucheli <jose.bucheli@correounivalle.edu.co>
2023-01-04 15:00:18 -0500

    fixing DB

--- server/src/database/database.sql
+++ server/src/database/database.sql
@@ -1,25 +1,13 @@
-CREATE TABLE user(
+CREATE TABLE client(
     phone VARCHAR(100),
     full_name VARCHAR(200),
     pw VARCHAR(100),
     address VARCHAR(200),
     email VARCHAR(100),
-    card_number INTEGER(20),
+    card_number INTEGER,
     PRIMARY KEY (phone)
 );
 
-
-CREATE TABLE request(
-    id SERIAL,
-    description VARCHAR(500),
-    state VARCHAR(100),
-    empl_phone VARCHAR(100),
-    user_phone VARCHAR(100),
-    id_work VARCHAR(100),
-    PRIMARY KEY(id),
-    FOREIGN KEY(empl_phone) REFERENCES employee (phone),
-    FOREIGN KEY(user_phone) REFERENCES user (phone)
-);
 
 CREATE TABLE employee (
     phone VARCHAR(100),
@@ -31,11 +19,30 @@ CREATE TABLE employee (
     PRIMARY KEY(phone)
 );
 
+CREATE TABLE request(
+    id SERIAL,
+    description VARCHAR(500),
+    state VARCHAR(100),
+    empl_phone VARCHAR(100),
+    user_phone VARCHAR(100),
+    id_work VARCHAR(100),
+    PRIMARY KEY(id),
+    FOREIGN KEY(empl_phone) REFERENCES employee (phone),
+    FOREIGN KEY(user_phone) REFERENCES client (phone)
+);
+
+CREATE TABLE work(
+    id SERIAL,
+    PRIMARY KEY(id),
+    name VARCHAR(100)
+);  
+
 CREATE TABLE employee_work(
     empl_phone VARCHAR(100),
-    id_work VARCHAR(100),
+    id_work SERIAL,
     paid INTEGER,
-    PRIMARY KEY(empl_phone),
-    PRIMARY KEY(id_work),
-    FOREIGN KEY(empl_phone) REFERENCES employee(phone)
+    PRIMARY KEY(empl_phone, id_work),
+    FOREIGN KEY(empl_phone) REFERENCES employee(phone),
+    FOREIGN KEY(id_work) REFERENCES work(id)
 );
+
