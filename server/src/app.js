5d4bc3086da8eb3fe479c1e2f49b1b0191ca75c8
GetialJuan <getial.juan@correounivalle.edu.co>
2023-01-05 17:32:53 -0500

    rutas usuario

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,23 @@
 //Logica de la Aplicacion
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
 
-const app = express()
+const app = express();
 
 //creamos una "variable" para el puerto y buscamos si existe alguna en las variables de entorno 
-app.set('port',process.env.PORT || 4000)
+app.set('port',process.env.PORT || 4000);
 
 //para usar en el scrip de dev
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 // para recibir json
 app.use(express.json());
 
 //cada vez que requiramos la ruta /api/trabajador  se trabajara con /routes/routes
-app.use("/api/trabajador",require('./routes/trabajador.routes'))
+app.use("/api/trabajador",require('./routes/trabajador.routes'));
+
+// Rutas de usuario
+app.use('/api/usuario', require('./routes/usuario.routes'));
  
 
 //exportamos el archivo
