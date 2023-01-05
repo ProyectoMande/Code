351000d90e808be723690af7194cb931eabfa723
GetialJuan <getial.juan@correounivalle.edu.co>
2023-01-05 16:08:53 -0500

    rutas trabajdor

---
+++ server/src/routes/routes.js
@@ -0,0 +1,35 @@
+// Rutas que permitiran definir las 
+// operaciones del servidor
+
+// Se importa express
+const express = require('express');
+
+// Objeto que permite ingresar rutas
+const router = express.Router();
+
+// COntroladores
+const trabajadorCtrl = require('../controllers/trabajador.controllers');
+
+// Establecemos que se hara en cada ruta //
+
+// Ruta principal (/)
+// req:request  res:response
+router.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+// Agregar ta¿rabajdor
+router.post('/trabajador', trabajadorCtrl.addTrabajador);
+
+// Ver trabajadores
+router.get('/trabajador', trabajadorCtrl.getTrabajador);
+
+// Eliminar trabajador
+router.delete('/trabajador', trabajadorCtrl.deleteTrabajador);
+
+// Actualizar trabajador
+router.put('/trabajador', trabajadorCtrl.updateTrabajador);
+
+// Se exporta el objeto router
+// Es decir, cuando se importe este archivo, se importara dicho objeto
+module.exports = router;
