5ec01072f9b821cdad3b4a95ddbfcf7509a72551
Bucheli <jose.bucheli@correounivalle.edu.co>
2023-01-05 18:34:38 -0500

    nav y routing working

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from './components/home/home.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
+
+
+//rutas de navegacion
+const routes: Routes = [
+  {path: '', redirectTo:'home',pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'user-register',component: UserRegisterComponent},
+  {path: 'employee',component: EmployeeComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './components/employee/employee.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
+import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    HomeComponent,
+    UserRegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
