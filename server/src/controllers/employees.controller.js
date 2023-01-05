94cff2ad4defc5f574c7660f6f41ed715d7e35ae
Bucheli <jose.bucheli@correounivalle.edu.co>
2023-01-05 09:22:07 -0500

    se cambio la consulta para que funcione al entrar a una pagina

--- server/src/controllers/employees.controller.js
+++ server/src/controllers/employees.controller.js
@@ -1,15 +1,25 @@
 //creo un objeto con funciones a exportar
 const empCtrl = {}
 
-empCtrl.getEmployees = (req,res) => {res.send('getting employees')} // multiples
+const {Pool}= require('pg');
+
+const pool = new Pool(
+    {
+        host: "localhost",
+        user: "bucheli",
+        port: 5432,
+        password: "0000",
+        database: "mandeDev"
+    }
+);
+
+empCtrl.getEmployees = async (req,res) => {
+    const response = await pool.query('SELECT * FROM employee');
+    console.log(response.rows);
+    res.send('getting employees')};
+
 empCtrl.createEmployee = (req,res) => {res.send('creating employees')}
 empCtrl.getEmployee = (req,res) => {res.send('getting employee')} //un empleado
 empCtrl.updateEmployee = (req,res) => {res.send('updating employees')}
 empCtrl.deleteEmployee = (req,res) => {res.send('deleating employees')}
 module.exports = empCtrl
-
-const {Pool} = require('pg');
-
-new Pool({
-    
-})
--- server/src/routes/employees.routes.js
+++ server/src/routes/employees.routes.js
@@ -8,7 +8,7 @@ const empCtrl =require('../controllers/employees.controller.js')
 //cuando queremos entrar a la ruta respondemos con una funcion del objeto empCtrl
 router.get('/',empCtrl.getEmployees)
 router.post('/',empCtrl.createEmployee) //enviar informacion
-router.get('/:id',empCtrl.getEmployee)
+router.get('/:id',empCtrl.getEmployee) //en el navegador http://localhost:4000/api/employees/:id
 router.put('/:id',empCtrl.updateEmployee) //actualizar
 router.delete('/:id',empCtrl.deleteEmployee) //borrar
 
