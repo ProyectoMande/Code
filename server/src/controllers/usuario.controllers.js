b7637e5c477d900e98157411bd5e01c53d9dccd0
GetialJuan <getial.juan@correounivalle.edu.co>
2023-01-05 18:02:04 -0500

    addUsuario lista

--- server/src/controllers/usuario.controllers.js
+++ server/src/controllers/usuario.controllers.js
@@ -2,8 +2,32 @@ const db = require('../database');
 
 const usuarioCtrl = {};
 
-usuarioCtrl.addUsuario = (req, res) => {
-    res.send('agregar usuario');
+usuarioCtrl.addUsuario = async (req, res) => {
+    // Datos del usuario a añadir
+    const {
+        celular,
+        nombreCompleto,
+        id,
+        email,
+        gps_latitud,
+        gps_longitud,
+        tarjeta_numero,
+        tarjeta_fecha_vencimiento,
+        tarjeta_cvv
+     } = req.body;
+    
+    // Agregamos el usuario a la db
+    const newUsuario = await db.query(`INSERT INTO usuario 
+        (celular, nombreCompleto, id, email, gps_latitud, 
+            gps_longitud, tarjeta_numero, tarjeta_fecha_vencimiento, tarjeta_cvv)
+            VALUES (
+                $1, $2, $3, $4, $5, $6, MD5($7), MD5($8), MD5($9)
+            ) RETURNING *`, 
+            [celular, nombreCompleto, id, email, gps_latitud, gps_longitud, 
+            tarjeta_numero, tarjeta_fecha_vencimiento, tarjeta_cvv]);
+
+    // Mostramos el nuevo usuario
+    console.log(newUsuario.rows[0]);
 };
 
 module.exports = usuarioCtrl;
