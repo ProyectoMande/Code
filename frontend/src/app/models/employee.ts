85b3b3cac728c6507a0702af8893c9e9d8bb9e9d
GetialJuan <getial.juan@correounivalle.edu.co>
2023-01-07 13:52:28 -0500

    modelo trabajdor

---
+++ frontend/src/app/models/employee.ts
@@ -0,0 +1,9 @@
+export interface Trabajador {
+    nombreCompleto: string,
+    celular: string,
+    id: string,
+    email: string,
+    direccion: string,
+    fotoId?: File,
+    fotoPerfil?: File
+}
