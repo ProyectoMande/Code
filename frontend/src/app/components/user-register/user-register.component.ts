63f783bbb3a2d0356c9f3a7704a41cfd7a17d19e
GetialJuan <getial.juan@correounivalle.edu.co>
2023-01-14 16:27:31 -0500

    registro usuario

--- frontend/src/app/components/user-register/user-register.component.ts
+++ frontend/src/app/components/user-register/user-register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from '../../models/usuario'
 
 @Component({
   selector: 'app-user-register',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class UserRegisterComponent {
 
+  constructor(){}
+
+  usuarioNuevo: Usuario = {
+    celular: '',
+    nombreCompleto: '',
+    id: '',
+    email: '',
+    direccion: '',
+    tarjeta_numero: '',
+    tarjeta_fecha_vencimiento: '',
+    tarjeta_cvv: ''
+  };
+
+  addUsuario(){
+    console.log(this.usuarioNuevo);
+  }
 }
---
+++ frontend/src/app/models/usuario.ts
@@ -0,0 +1,10 @@
+export interface Usuario {
+    celular: string,
+    nombreCompleto: string,
+    id: string,
+    email: string,
+    direccion: string,
+    tarjeta_numero: string,
+    tarjeta_fecha_vencimiento: string,
+    tarjeta_cvv: string
+}
