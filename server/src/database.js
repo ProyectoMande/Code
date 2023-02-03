bcb016d99ea6f4963477ca63acca13437a7011aa
Seb0927 <sebastianidrobo@hotmail.com>
2023-02-02 19:26:03 -0500

    Me hackearon y por eso el commit anterior estaba malo - Disculpen las molestias :+1:

--- server/src/database.js
+++ server/src/database.js
@@ -2,10 +2,10 @@ const { Pool }= require('pg');
 
 const db = new Pool(
     {
-        host: "database",
+        host: "172.17.0.2",
         user: "postgres",
         port: 5432,
-        password: "postgres",
+        password: "postgis",
         database: "postgres"
     }
 );
